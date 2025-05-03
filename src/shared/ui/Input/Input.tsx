6c721bfacadb04fa14003aa148aa817bb84796bb
Бондаренко Олег <mcaleg2006@Noutbuk-Bondarenko.local>
2025-05-03 18:18:22 +0300

    Pre-version-autorized

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,28 +1,47 @@
 import { TextInput } from "react-native-paper"
 import { InputProps } from "./type"
-import { View } from "react-native";
+import { View,Image } from "react-native";
 import { StyleSheet } from "react-native";
+import React, {useState} from 'react';
 
+// const eyeIcon = require('../../../assets/images/eye.png');
 
-
-
-export const Input: React.FC<InputProps> = ({ typePage, field, placeholder, type = "default" }) => {
+export const Input: React.FC<InputProps> = ({typePage, field, placeholder, type = "default",secureTextEntry = false,showEyeIcon = false }) => {
+  const [isSecure,setIsSecure] = useState(secureTextEntry)
   return (
     <View style={{ alignItems: "center" }}>
       <TextInput style={[
-
         styles.input,
-        typePage === 'login' ? styles.login : styles.register
+        typePage === 'login' ? styles.login : styles.register,
+          { borderRadius: 5 }
       ]}
-        keyboardType={type}
-                 underlineColor="transparent"
-                 selectionColor="transparent"
-                 outlineColor="transparent"
+                 keyboardType={type}
+                 mode="flat"
                  label={placeholder}
-        value={field.value}
-        onChangeText={field.onChange}
-        theme={{ primary: 'blue', underlineColor: 'transparent', roundness: 110 }}
-        {...field}
+                 value={field.value}
+                 onChangeText={field.onChange}
+                 secureTextEntry={isSecure}
+
+                 theme={{
+                     colors: {
+                         primary: 'orange',
+                         background: 'white',
+                         placeholder: 'red',
+                         text: 'black',
+                     },
+                     // roundness: 4
+      }}
+                 underlineColor="transparent"
+                 activeOutlineColor="transparent"
+                 right={
+                     showEyeIcon && (
+                         <TextInput.Icon
+                             icon={isSecure ? 'eye-off' : 'eye'}
+                             onPress={() => setIsSecure(!isSecure)}
+                         />
+                     )
+                 }
+                 {...field}
       />
     </View>
   )
@@ -31,17 +50,13 @@ export const Input: React.FC<InputProps> = ({ typePage, field, placeholder, type
 
 const styles = StyleSheet.create({
   input: {
-    width: 330,
-    height: 55,
-    backgroundColor: 'white',
-    color: '',
-
-    paddingHorizontal: 15,
-    marginBottom:10,
-
-    borderWidth: 1,
-    borderRadius: 110,
-
+      width: 330,
+      height: 55,
+      backgroundColor:'white',
+      paddingHorizontal: 15,
+      marginBottom:20,
+      borderWidth: 1,
+      borderColor:'#eee',
   },
   login: {
 
